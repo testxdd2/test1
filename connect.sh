0ea0122b68c2244587f9fb8f8b30dd4f9c310e97
testxdd2 <87915807+testxdd2@users.noreply.github.com>
2024-05-20 13:10:43 -0600

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,20 +1,8 @@
-TOR_UID="debian-tor"
-TOR_PORT="9050"
-
-
-iptables -t nat -A OUTPUT -m owner --uid-owner $TOR_UID -j RETURN
+iptables -t nat -A OUTPUT -m owner --uid-owner debian-tor -j RETURN
 iptables -t nat -A OUTPUT -p udp --dport 53 -j REDIRECT --to-ports 53
-iptables -t nat -A OUTPUT -p tcp --dport 53 -j REDIRECT --to-ports 53
-iptables -t nat -A OUTPUT -p udp -m owner --uid-owner $TOR_UID -m udp --dport 53 -j REDIRECT --to-ports 53
-	
-iptables -t nat -A OUTPUT -p tcp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
-iptables -t nat -A OUTPUT -p udp -d 10.192.0.0/10 -j REDIRECT --to-ports 9040
+iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports 9040
 
-iptables -t nat -A OUTPUT -p tcp --syn -j REDIRECT --to-ports $TOR_PORT
-iptables -t nat -A OUTPUT -p udp -j REDIRECT --to-ports $TOR_PORT
-iptables -t nat -A OUTPUT -p icmp -j REDIRECT --to-ports $TOR_PORT
-	
+# Permitir tráfico de Tor
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-iptables -A OUTPUT -m owner --uid-owner $TOR_UID -j ACCEPT
+iptables -A OUTPUT -m owner --uid-owner debian-tor -j ACCEPT
 iptables -A OUTPUT -j REJECT
