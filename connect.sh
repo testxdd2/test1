4a9f24908653203ddc49d17cf8b9962ffb57dcab
testxdd2 <87915807+testxdd2@users.noreply.github.com>
2024-05-20 13:02:53 -0600

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,2 +1,10 @@
-iptables -t nat -A OUTPUT -p TCP --dport 80 -j REDIRECT --to-destination 127.0.0.1:9050
-iptables -t nat -A OUTPUT -p TCP --dport 443 -j REDIRECT --to-destination 127.0.0.1:9050
+# Configurar el tráfico TCP para redirigirlo a Tor
+sudo iptables -t nat -A OUTPUT -m owner --uid-owner <tu_usuario> -p tcp --syn -j REDIRECT --to-ports 9040
+
+# Configurar el tráfico DNS para redirigirlo a Tor
+sudo iptables -t nat -A OUTPUT -m owner --uid-owner <tu_usuario> -p udp --dport 53 -j REDIRECT --to-ports 5353
+
+# Configurar la cadena OUTPUT para que no afecte al propio Tor
+sudo iptables -t nat -A OUTPUT -m owner --uid-owner debian-tor -j RETURN
+sudo iptables -t nat -A OUTPUT -p tcp --dport 9040 -j RETURN
+sudo iptables -t nat -A OUTPUT -p udp --dport 5353 -j RETURN
